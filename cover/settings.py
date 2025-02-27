da1eba85b647981e5c23aa53d8e1fb7884a7fbdf
crepantherx <crepantherx@gmail.com>
2025-02-27 11:54:26 +0530

    very good

--- cover/settings.py
+++ cover/settings.py
@@ -123,9 +123,13 @@ USE_TZ = True
 # STATIC_URL = '/static/'
 # STATICFILES_DIRS = [BASE_DIR / "app/static"]
 
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATIC_URL = '/staticfiles/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'app/static')]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATIC_URL = '/staticfiles/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'app/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
