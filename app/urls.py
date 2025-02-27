8784cf1d153a6b8ca58e18f56a54c173c66e3d44
crepantherx <crepantherx@gmail.com>
2025-02-27 09:51:21 +0530

    good

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('services', views.services, name='services'),
+    path('demo', views.demo, name='demo'),
+    path('legal', views.legal, name='legal'),
+    path('login', views.login, name='login'),
+    path('careers', views.careers, name='careers'),
+    path('about', views.about, name='about'),
+
 ]
