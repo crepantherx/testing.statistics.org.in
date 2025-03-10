8c6ef4bc1ee68632761afe1fdd4bc58a0874080b
crepantherx <crepantherx@gmail.com>
2025-03-10 18:36:14 +0530

    library

--- api/settings.py
+++ api/settings.py
@@ -47,10 +47,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware"
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = "api.urls"
 
